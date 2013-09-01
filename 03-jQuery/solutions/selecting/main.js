62b43b57be8c332a08ad54003301aabfdbb59443
marcbaechinger <marc.baechinger@gmail.com>
2013-09-01 22:32:05 +0200

    [03-jquery]: prepared basic selection exercise

--- 03-jQuery/solutions/selecting/main.js
+++ 03-jQuery/solutions/selecting/main.js
@@ -9,7 +9,14 @@
 	//  (<div class="image-container"><section/></div>)
 	$(".image-container section").addClass("badge");
 	
-	// task 4 ==> select the 'section' element which is a direct child of the body
+	// task 4 ==> select the img element inside the 4th .image-container element
+	// <div class="image-container"></div>
+	// <div class="image-container"></div>
+	// <div class="image-container"></div>
+	// <div class="image-container"><img></div>   <===== 4th .image-container
+	$(".image-container:nth-child(4) > img").addClass("badge");
+	
+	// task 5 ==> select the 'section' element which is a direct child of the body
 	$("body > section").removeClass("badge");
 	
 	// check resulting browser view: 4 badges with a football player are shown in a grid
