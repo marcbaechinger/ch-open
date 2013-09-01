8987836fde56dd1c89938d172e59069d89c2d7d7
marcbaechinger <marc.baechinger@gmail.com>
2013-09-02 00:02:45 +0200

    [03-jquery]: prepared exercise about custom events.

--- 03-jQuery/exercises/events/custom/main.js
+++ 03-jQuery/exercises/events/custom/main.js
@@ -8,22 +8,41 @@
 		var target = $(ev.target),
 			action = target.data("action");
 			
-		if (action === "save") {
-			// task: fire a custom event
-			target.trigger("update.myApp", {
-				password: dialog.find("[name=password]").val(),
-				email: dialog.find("[name=email]").val(), 
-			});
-		} else if (action === "cancel") {
-			dialog.find("input").val("");
+		if (action === "cancel") {
+			// task-1: trigger custom event 'cancel.myApp'
+			// hint: http://api.jquery.com/trigger/
+			// [...]
+		} else if (action === "save") {
+			// task-2: fire a custom event 'save.myApp' and pass an object 
+			//         with 'password' and 'email' properties. Get the values
+			//         of the properties from the UI (input elements)
+			// hint: http://api.jquery.com/trigger/
+			// [...]
+			// [...]
+			// [...]
+			// [...]
 		}
 		
 		ev.preventDefault();
 		ev.stopPropagation();
 	});
 	
+
+	// task-1: listen to 'cancel.myApp' event and clear all input fields
+	// [...]
+	// [...]
+	// [...]
 	
-	dialog.on("update.myApp", function (ev, data) {
+	// listens the custom 'save.myApp' event
+	dialog.on("save.myApp", function (ev, data) {
 		alert("custom: " + JSON.stringify(data));
 	});
+	
+	
+	/*
+	assert in webbrowser:
+	=========================
+		(task-1) on clicking the cancel-button the dialog form is cleared
+		(task-2) on clicking the save-button an alert shows the data of all form inputs
+	*/
 }(this));
