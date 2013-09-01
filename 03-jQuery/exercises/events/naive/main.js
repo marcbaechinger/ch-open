e270505240f3af949212cb3659dd42405ba6384b
marcbaechinger <marc.baechinger@gmail.com>
2013-09-01 23:00:20 +0200

    [03-jquery]: prepared naive event handling exercises

--- 03-jQuery/exercises/events/naive/main.js
+++ 03-jQuery/exercises/events/naive/main.js
@@ -1,16 +1,49 @@
 (function (global) {
-	// task: select the left image (class=trigger)
-	var trigger = $(".trigger");
+	// task: select the left (class=trigger) and right image (class=target) 
+	var trigger = $("REPLACE_ME"),
+		target = $("REPLACE_ME");
 	
-	// task: register mouse over event
-	trigger.on("mouseover", function (ev) {
-		// add class 'animate' to right image
-		$(".target").addClass("animate");
+	
+	// task-1: register mouseover event to trigger using jquery api
+	// ---------------------------------------------
+	// hint: trigger.on(...)
+	// see: http://api.jquery.com/on/
+	// [...] INSERT HERE
+		// add class 'animate' to target image: must be called on mouse over
+		target.addClass("animate");
+	// [...] INSERT HERE
+	
+	
+	
+	// task-2: register mouseout event to trigger
+	// ---------------------------------------------
+	// [...] INSERT HERE
+		// add class 'animate' to target image: must be called on mouse out
+		target.removeClass("animate");
+	// [...] INSERT HERE
+	
+	
+	
+	// task-3: stop propagation and default processing on click
+	// ---------------------------------------------
+	$("img").on("click", function (ev) {
+		// INSERT HERE: avoid further propagation of event
+		// [...]
+		// see: http://api.jquery.com/event.preventDefault/
+		// INSERT HERE: avoid processing of default action 
+		// [...]
+		// see: http://api.jquery.com/event.stopPropagation/
 	});
 	
-	// task: register mouse out event
-	trigger.on("mouseout", function (ev) {
-		// add class 'animate' to right image
-		$(".target").removeClass("animate");
+	$(document).on("click", "img", function (ev) {
+		alert("Verhindere das Propagieren der Events");
 	});
+	
+	/*
+	assert in webbrowser:
+	=========================
+		(task-1) hover over the left image lets the right image start to spin
+		(task-2) right image stops spinning on mouse out of the left image
+		(task-3) clicking the left image does not show an alert bar
+	*/
 }(this));
--- 03-jQuery/solutions/events/naive/main.js
+++ 03-jQuery/solutions/events/naive/main.js
@@ -1,16 +1,32 @@
 (function (global) {
-	// task: select the left image (class=trigger)
-	var trigger = $(".trigger");
+	// task: select the left (class=trigger) and right image (class=target) 
+	var trigger = $(".trigger"),
+		target = $(".target");
 	
-	// task: register mouse over event
-	trigger.on("mouseover", function (ev) {
-		// add class 'animate' to right image
-		$(".target").addClass("animate");
+	// task-1: register mouseover event to trigger using jquery api
+	// ---------------------------------------------
+	trigger.on("mouseover", function (e) {
+		// add class 'animate' to target image: must be called on mouse over
+		target.addClass("animate");
 	});
 	
-	// task: register mouse out event
-	trigger.on("mouseout", function (ev) {
-		// add class 'animate' to right image
-		$(".target").removeClass("animate");
+	
+	// task-2: register mouseout event to trigger
+	// ---------------------------------------------
+	trigger.on("mouseout", function () {
+		// add class 'animate' to target image: must be called on mouse out
+		target.removeClass("animate");
+	});
+	
+	
+	// task-3: stop propagation and default processing on click
+	// ---------------------------------------------
+	$("img").on("click", function (ev) {
+		ev.stopPropagation(); // avoid further propagation of event
+		ev.preventDefault(); // avoid processing of default action 
+	});
+	
+	$(document).on("click", "img", function (ev) {
+		alert("Verhindere das Propagieren der Events");
 	});
 }(this));
