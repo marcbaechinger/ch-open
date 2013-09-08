13601c86d5159b2dfa2abd0bae90c2d125f28315
jbandi <dev@jonasbandi.net>
2013-09-08 18:16:13 +0200

    Cleanup test for linting

--- 01-FirstHandsOn/02-SimplisticToDo-refactored/js/test/todoSpec.js
+++ 01-FirstHandsOn/02-SimplisticToDo-refactored/js/test/todoSpec.js
@@ -1,24 +1,26 @@
-'use strict';
-
-var input = $("<input type='text' id='input'/>");
-var addBtn = $('<a id="addBtn" class="btn btn-primary btn-large" >Add &raquo;</a>');
-var todoList = $('<p id="do"> </p>');
-$(document.body).append(input);
-$(document.body).append(addBtn);
-$(document.body).append(todoList);
-
+/*global $, describe, beforeEach, afterEach, it, expect, addText, registerButtonHandler */
 describe('ToDo List', function(){
+    'use strict';
 
     beforeEach(function() {
+
+        // Set up dom for test
+        var input = $("<input type='text' id='input'/>");
+        var addBtn = $('<a id="addBtn" class="btn btn-primary btn-large" >Add &raquo;</a>');
+        var todoList = $('<p id="do"> </p>');
+        $(document.body).append(input);
+        $(document.body).append(addBtn);
+        $(document.body).append(todoList);
+
         // Hack: Firefox does not properly register event handlers when loaded in Jasmine
-        if (navigator.userAgent.indexOf("Firefox") != -1 && !$._data($('#addBtn').get(0),'events') ){
+        if (navigator.userAgent.indexOf("Firefox") !== -1 && !$._data($('#addBtn').get(0),'events') ){
             registerButtonHandler();
         }
     });
 
     afterEach(function() {
         $('#do').empty();
-    })
+    });
 
     it('should extend list when adding item ', function() {
         var input = $('input');
@@ -28,7 +30,7 @@ describe('ToDo List', function(){
 
         var todoList = $('#do h3');
         expect(todoList.length).toBe(1);
-        expect(todoList.eq(0).text()).toBe('First ToDo')
+        expect(todoList.eq(0).text()).toBe('First ToDo');
     });
 
     it('should add item when clicking button', function() {
@@ -43,3 +45,5 @@ describe('ToDo List', function(){
         expect(itemCount).toBe(1);
     });
 });
+
+
