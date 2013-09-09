9c284793e5307dfb0e8fa1d4efca547df0e359f2
jbandi <dev@jonasbandi.net>
2013-09-10 00:03:26 +0200

    Koans for inheritance

--- 02-Language/01-Lessons/7-Inheritance/AboutInheritance.js
+++ 02-Language/01-Lessons/7-Inheritance/AboutInheritance.js
@@ -1,85 +1,147 @@
-function Muppet(age, hobby) {
-  this.age = age;
-  this.hobby = hobby;
+APP = {};
 
-  this.answerNanny = function(){
-	return "Everything's cool!";
-  }
-}
+lesson("about classical inheritance", function() {
+    beforeEach(function(){
 
-function SwedishChef(age, hobby, mood) {
-  Muppet.call(this, age, hobby);
-  this.mood = mood;
+        APP.Pet = function (name) {
+            this.getName = function() { return name; };
+            this.setName = function(newName) { name = newName; };
+        };
+        APP.Pet.prototype.toString = function() {
+            return "This pet's name is: " + this.getName();
+        };
 
-  this.cook = function() {
-    return "Mmmm soup!";
-  }
-}
+        APP.Cat = function (name, color) {
+            APP.Pet.call(this, name); // borrow the constructor
+            this.getColor = function() { return color; };
+        };
+        APP.Cat.prototype = new APP.Pet();
 
-SwedishChef.prototype = new Muppet();
+        APP.Dog = function (name, breed) {
+            APP.Pet.call(this, name); // borrow the constructor
+            this.getBreed = function() { return breed; };
+        };
+        APP.Dog.prototype = new APP.Pet();
+        APP.Dog.prototype.toString = function() {
+            return "This dog's name is: " + this.getName() + ", and its breed is: " + this.getBreed();
+        };
 
-lesson("About inheritance", function() {
-  beforeEach(function(){
-    this.muppet = new Muppet(2, "coding");
-	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
-  });
+        this.myPet = new APP.Pet("Squishy");
+        this.myCat = new APP.Cat("Java", "black");
+        this.myDog = new APP.Dog("Bello", "Pudel");
+    });
 
-  learn("about calling a method on the derived object", function() {
-    expect(this.swedishChef.cook()).toEqual(FILL_ME_IN);
-  });
+    learn("about types of derived objects", function(){
+        expect(this.myPet instanceof APP.Pet).toBe(FILL_ME_IN);
+        expect(this.myCat instanceof APP.Pet).toBe(FILL_ME_IN);
+        expect(this.myDog instanceof APP.Pet).toBe(FILL_ME_IN);
+        expect(this.myCat instanceof APP.Cat).toBe(FILL_ME_IN);
+        expect(this.myDog instanceof APP.Dog).toBe(FILL_ME_IN);
+        expect(this.myDog instanceof APP.Cat).toBe(FILL_ME_IN);
+    });
 
-  learn("about callinf a method on the base object", function() {
-    expect(this.swedishChef.answerNanny()).toEqual(FILL_ME_IN);
-  });
+    learn("about calling derived methods on objects", function() {
+        expect(this.myPet.toString()).toEqual(FILL_ME_IN);
+        expect(this.myCat.toString()).toEqual(FILL_ME_IN);
+        expect(this.myDog.toString()).toEqual(FILL_ME_IN);
+    });
 
-  learn("about setting constructor parameters on the base object", function() {
-    expect(this.swedishChef.age).toEqual(FILL_ME_IN);
-    expect(this.swedishChef.hobby).toEqual(FILL_ME_IN);
-  });
+    learn("about extending the prototype of an object", function() {
+        APP.Cat.prototype.toString = function(){
+            return "This cat's name is: " + this.getName() + ", and its color is: " + this.getColor();
+        };
+        expect(this.myCat.toString()).toEqual(FILL_ME_IN);
+    });
 
-  learn("about setting constructor parameters on the derived object", function() {
-    expect(this.swedishChef.mood).toEqual(FILL_ME_IN);
-  });
+    learn("that properties defined on the object precede properties on the prototype", function() {
+        this.myDog.toString = function(){
+            return "Wuff wuff wuff!";
+        };
+        expect(this.myDog.toString()).toEqual(FILL_ME_IN);
+    });
+
+    learn("that changing the prototype of the base class propagates to derived classes", function() {
+        this.myPet.toString = function(){
+            return "Squish squish squish";
+        };
+
+        APP.Pet.prototype.toString = function(){
+            return "Squash squash squash";
+        };
+
+        expect(this.myPet.toString()).toEqual(FILL_ME_IN);
+        expect(this.myCat.toString()).toEqual(FILL_ME_IN);
+    });
+
+    learn("about changing the protoype", function(){
+        APP.Cat.prototype = {};
+
+        var secondCat = new APP.Cat();
+
+        expect(this.myCat.toString()).toBe(FILL_ME_IN);
+        expect(secondCat.toString()).toBe(FILL_ME_IN);
+
+    })
 });
 
-// http://javascript.crockford.com/prototypal.html
-Object.prototype.beget = function () {
-  function F() {}
-  F.prototype = this;
-  return new F();
-}
+lesson("About prototypal inheritance", function() {
+    beforeEach(function(){
 
-function Gonzo(age, hobby, trick) {
-  Muppet.call(this, age, hobby);
-  this.trick = trick;
+        APP.Pet = function () {
+            this.name = "Anonymous";
+            this.getName = function() { return this.name; };
+        };
+        APP.Pet.prototype.toString = function() {
+            return "This pet's name is: " + this.getName();
+        };
 
-  this.doTrick = function() {
-    return this.trick;
-  }
-}
+        this.myPet = new APP.Pet();
+        this.myDog = Object.create(this.myPet);
 
-//no longer need to call the Muppet (base type) constructor
-Gonzo.prototype = Muppet.prototype.beget();
+        this.myDog.name = "Bello";
+    });
 
-lesson("About Crockford's inheritance improvement", function() {
-  beforeEach(function(){
-  this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
-  });
+    learn("about types of derived objects", function(){
+        expect(this.myPet instanceof APP.Pet).toBe(FILL_ME_IN);
+        expect(this.myDog instanceof APP.Pet).toBe(FILL_ME_IN);
 
-  learn("be able to call a method on the derived object", function() {
-    expect(this.gonzo.doTrick()).toEqual(FILL_ME_IN);
-  });
+        var myPudel = Object.create(this.myDog);
+        expect(myPudel instanceof APP.Pet).toBe(FILL_ME_IN);
+    });
 
-  learn("should be able to call a method on the base object", function() {
-    expect(this.gonzo.answerNanny()).toEqual(FILL_ME_IN);
-  });
+    learn("about derived properties", function() {
+        expect(this.myPet.getName()).toEqual(FILL_ME_IN);
+        expect(this.myDog.getName()).toEqual(FILL_ME_IN);
+        expect(this.myPet.toString()).toEqual(FILL_ME_IN);
+        expect(this.myDog.toString()).toEqual(FILL_ME_IN);
 
-  learn("should set constructor parameters on the base object", function() {
-    expect(this.gonzo.age).toEqual(FILL_ME_IN);
-    expect(this.gonzo.hobby).toEqual(FILL_ME_IN);
-  });
+        var myPudel = Object.create(this.myDog);
+        expect(myPudel.getName()).toEqual(FILL_ME_IN);
 
-  learn("should set constructor parameters on the derived object", function() {
-    expect(this.gonzo.trick).toEqual(FILL_ME_IN);
-  });
+        myPudel.name = "Cornelius";
+        expect(myPudel.getName()).toEqual(FILL_ME_IN);
+        expect(this.myDog.getName()).toEqual(FILL_ME_IN);
+    });
+
+    learn("about the prototype chain", function() {
+        expect(Object.getPrototypeOf(this.myDog)).toBe(this.myPet);
+
+        var myPudel = Object.create(this.myDog);
+        expect(Object.getPrototypeOf(myPudel)).toBe(FILL_ME_IN);
+        expect(Object.getPrototypeOf(Object.getPrototypeOf(myPudel))).toBe(FILL_ME_IN);
+    });
+
+    learn("that prototypal inheritance works well with object literals", function() {
+        var animal = { eats: true };
+        var rabbit = Object.create(animal);
+
+        expect(rabbit.eats).toBe(FILL_ME_IN);
+        expect(rabbit.hasOwnProperty("eats")).toBe(FILL_ME_IN);
+
+        rabbit.eats = false;
+
+        expect(rabbit.eats).toBe(FILL_ME_IN);
+        expect(animal.eats).toBe(FILL_ME_IN);
+        expect(rabbit.hasOwnProperty("eats")).toBe(FILL_ME_IN);
+    });
 });
